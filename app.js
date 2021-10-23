00cc6b2a951e63e65f25790125866ab4b17a68af
Dinesh Rout <rddinesh779@gmail.com>
2021-10-23 12:04:03 +0530

    db connection added

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ import Mongoose from 'mongoose';
 const app = express();
 const PORT = 3000;
 
-const url = ''
+const url =
+  'mongodb+srv://biswajit:b7D7ehi28LWSQRmw@cluster0.epz3m.mongodb.net/coin_store?retryWrites=true&w=majority';
+
+Mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('database conncted!');
+    app.listen(PORT, () => {
+      console.log(`server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello! Its working!.</h1>');
-});
-
-app.listen(PORT, () => {
-  console.log(`server running on http://localhost:${PORT}`);
 });
